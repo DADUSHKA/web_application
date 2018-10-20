471d2864778999a205f2856dd53d4df3c72fb59c
vicstor <vicstor@bk.ru>
2018-10-20 23:43:47 +0300

    made models

---
+++ db/migrate/20181020204111_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.string :category
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
