434311ae6db3117187ca5810a6da4ae09283c786
vicstor <vicstor@bk.ru>
2018-10-23 22:29:38 +0300

    lesson_fore is ready

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.tests_of_certain_categories(name_category)
+    self.where(category_id: Category.where(title: name_category)
+               .ids).order('id DESC').pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def list_of_user_tests_by_difficulty(value)
+    Test.where(level: value).pluck(:title).join("' , '")
+  end
 end
