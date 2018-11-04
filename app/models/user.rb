eea5687131a7efaf7f58132b4c9291f17827eb66
vicstor <vicstor@bk.ru>
2018-11-04 14:48:55 +0300

    lessons six misstake3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_many :tests_users
   has_many :tests, through: :tests_users
   has_many :author_tests, class_name: 'Test', foreign_key: "author_id"
 
   before_save { self.email = email.downcase }
+
   validates :name, :login_status, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
