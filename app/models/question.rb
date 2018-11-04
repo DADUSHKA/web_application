bea04dcaccff28c9f7eb1032b8ace46db6dc4281
vicstor <vicstor@bk.ru>
2018-11-04 12:41:30 +0300

    lessons six misstake3

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,11 +3,11 @@ class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
 
-  validate :validate_count_answer
+  validate :validate_count_answers
 
   private
 
-  def validate_count_answer
+  def validate_count_answers
     if answers.count > 4
       errors.add(:base, "One Question can have from 1 to 4 answers.")
     end
