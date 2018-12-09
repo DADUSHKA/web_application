278cb4edf149060ec01262b27cc23a400e6e37d5
vicstor <vicstor@bk.ru>
2018-12-09 12:28:51 +0300

    Lesson_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :tests do
-    resources :questions, shallow: true
+ resources :tests do
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
