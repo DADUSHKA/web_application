5189e0b0af30e8499b875b4c200f3c87562659ec
vicstor <vicstor@bk.ru>
2018-12-22 08:30:52 +0300

    Lesson nine mature misstake_7

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -2,6 +2,8 @@
 
 module TestPassagesHelper
 
+  TEST_THRESHOLD = 85
+
   def result_message
     selection("The  #{@test_passage.test.title} Test was Completed.",
               "The  #{@test_passage.test.title} Test no Completed.")
@@ -14,7 +16,7 @@ module TestPassagesHelper
   private
 
   def selection(str_1, str_2)
-    if @test_passage.success_rate >= 85
+    if @test_passage.success_rate >= TEST_THRESHOLD
       str_1
     else
       str_2
