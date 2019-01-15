b6c4b0e173368ce11a4de471d04fdaa9b33fc87e
Victor <33927289+DADUSHKA@users.noreply.github.com>
2019-01-15 09:12:18 +0100

    Update 20190110213655_add_first_name_last_name_to_users.rb

--- db/migrate/20190110213655_add_first_name_last_name_to_users.rb
+++ db/migrate/20190110213655_add_first_name_last_name_to_users.rb
@@ -1,8 +1,7 @@
 class AddFirstNameLastNameToUsers < ActiveRecord::Migration[5.2]
-  def change_column :users do |t|
-      t.string :first_name,  null: false, default: ''
-      t.string :last_name, null: false, default: ''
-    end
-    remove_column(:users, :name)
+   change_table :users do |t|
+    t.remove :name
+    t.string :first_name,  null: false, default: ''
+    t.string :last_name, null: false, default: ''
   end
 end
