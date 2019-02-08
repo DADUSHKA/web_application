33cd2b1765d78c31604bed89bdb2f1cc9642cf08
Ваше Имя <vicstor@bk.ru>
2019-02-08 21:38:08 +0300

    let->var

--- app/assets/javascripts/utilites/progress_bar.js
+++ app/assets/javascripts/utilites/progress_bar.js
@@ -1,11 +1,11 @@
 document.addEventListener("turbolinks:load", function(){
-  let startProgressBar = document.querySelector('.my-progressBar')
+  var startProgressBar = document.querySelector('.my-progressBar')
   if ( startProgressBar) {
-    let valueProgressBar = document.querySelector('#progressBar').value
-    let number_question = document.querySelector('.nuber-question').dataset.nuberQuestion
-    let amount_questions = document.querySelector('.all-question').dataset.allQuestion
+    var valueProgressBar = document.querySelector('#progressBar').value
+    var number_question = document.querySelector('.nuber-question').dataset.nuberQuestion
+    var amount_questions = document.querySelector('.all-question').dataset.allQuestion
 
-    let progressDivision =  Math.round(number_question * 100 / amount_questions)
+    var progressDivision =  Math.round(number_question * 100 / amount_questions)
 
      document.querySelector('#progressBar').value = valueProgressBar + progressDivision
   }
