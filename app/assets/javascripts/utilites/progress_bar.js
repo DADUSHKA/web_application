32a48d9872a3248ddda119d6b20edad225ca1967
Ваше Имя <vicstor@bk.ru>
2019-02-09 13:15:34 +0300

    corrections

--- app/assets/javascripts/utilites/progress_bar.js
+++ app/assets/javascripts/utilites/progress_bar.js
@@ -5,7 +5,7 @@ document.addEventListener("turbolinks:load", function(){
     var number_question = document.querySelector('.nuber-question').dataset.nuberQuestion
     var amount_questions = document.querySelector('.all-question').dataset.allQuestion
 
-    var progressDivision =  Math.round(number_question * 100 / amount_questions)
+    var progressDivision =  Math.round((number_question - 1) * 100 / amount_questions)
 
      document.querySelector('#progressBar').value = valueProgressBar + progressDivision
   }
